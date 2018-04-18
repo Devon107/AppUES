e7cc53dea22f0b9c0e7bff57da96c3b0fdc89644
Abinadi <abiclank@gmail.com>
2018-04-17 18:12:13 -0600

    Add files via upload

--- app/src/main/java/sv/edu/ues/uesrrii/PrincipalActivity.java
+++ app/src/main/java/sv/edu/ues/uesrrii/PrincipalActivity.java
@@ -1,8 +1,10 @@
 package sv.edu.ues.uesrrii;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,15 +20,16 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        Button btn = (Button) findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Intent ListMaterias = new Intent(getApplicationContext(), MateriasActivity.class);
+                startActivity(ListMaterias);
+            }
+        });
 
-    }
-
-    public void prueba1(View v){
-        TextView tw = findViewById(R.id.btnEconomia);
-        List<Pregunta> p = new ArrayList<>();
-
-        
 
     }
 }
